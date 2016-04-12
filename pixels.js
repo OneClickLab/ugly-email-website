732c8c56a7f7f7d39fee8f6b49c97e9e282ecd7d
Sonny T <mail@sonnyt.com>
2016-04-11 22:27:39 -0400

    Create pixels.js

---
+++ pixels.js
@@ -0,0 +1,15 @@
+window.UglyEmailTracker = {
+    '/track/open.php?u=': 'MailChimp',
+    '/wf/open?upn=': 'SendGrid',
+    'dripemail2': 'Drip',
+    'mailfoogae.appspot.com': 'Streak',
+    'bl-1.com': 'Bananatag',
+    't.yesware.com': 'Yesware',
+    'pstmrk.it/open': 'Postmark',
+    't.signauxtrois.com': 'Sidekick',
+    'tinyletterapp.com': 'TinyLetter',
+    'email.mixmax.com': 'MixMax',
+    'mailtrack.io': 'MailTrack',
+    'toutapp.com': 'toutapp',
+    'emltrk.com': 'Litmus'
+};
