2969faebf48f4e240b6229d7b992bdd45b4ab25b
Sonny T <mail@sonnyt.com>
2016-04-13 22:30:09 -0400

    Update pixels.js

--- pixels.js
+++ pixels.js
@@ -7,6 +7,7 @@ window.UglyEmailTracker = {
     't.yesware.com': 'Yesware',
     'pstmrk.it/open': 'Postmark',
     't.signauxtrois.com': 'Sidekick',
+    'sidekickopen': 'Sideckick',
     'tinyletterapp.com': 'TinyLetter',
     'email.mixmax.com': 'MixMax',
     'mailtrack.io': 'MailTrack',
