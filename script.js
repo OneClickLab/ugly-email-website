8f8ec444d858a48dde1e94113b3cff4b121dbbe9
Sonny T <mail@sonnyt.com>
2020-12-04 09:35:37 -0500

    clean up

--- script.js
+++ script.js
@@ -19,17 +19,26 @@ var app = new Vue({
       document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
     }
   },
-  created() {
-    fetch('https://trackers.uglyemail.com/list.txt')
-      .then(function(request) {
-        return request.text();
-      })
-      .then(function(res) {
-        var pixels = res.split('\n').map(function(row) {
-          return row.split('@@=')[0];
-        });
+  created: function() {
+    var fetchList = function(callback) {
+      var xmlhttp = new XMLHttpRequest();
 
-        this.allPixels = pixels.sort();
-      }.bind(this));
+      xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+          callback(xmlhttp.responseText);
+        }
+      };
+
+      xmlhttp.open('GET', 'https://trackers.uglyemail.com/list.txt', true);
+      xmlhttp.send();
+    };
+
+    fetchList(function(res) {
+      var pixels = res.split('\n').map(function(row) {
+        return row.split('@@=')[0];
+      });
+
+      this.allPixels = pixels.sort();
+    }.bind(this));
   }
 });
